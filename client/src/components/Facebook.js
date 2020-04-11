61d9173bd62eca7934af7230b9766a20d8f347fe
philipipara <54330369+philipipara@users.noreply.github.com>
2020-04-10 18:26:41 -0700

    Facebook login

---
+++ client/src/components/Facebook.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import FacebookLoginBtn from "react-facebook-login";
+
+ class Facebook extends Component {
+    state = {
+        auth: false,
+        name: "",
+        picture: ""
+    }
+
+    componentClicked = () => {
+        console.log("button clicked");
+    }
+    
+    responseFacebook = (response) => {
+        if(response.status !== "unknown")
+        this.setState({
+            auth: true,
+            name: response.name,
+            picture: response.picture.data.url,
+            email: response.email
+
+        });
+    }
+    render(){
+        let facebookData;
+        
+        this.state.auth ?
+            facebookData = (
+                <div>
+                    <img src={this.state.picture} alt={this.state.name} />
+                    <h2>Welcome {this.state.name}</h2>
+                    <p>{this.state.email}</p>
+                    
+                </div>
+            ) :
+
+            facebookData = (
+                <FacebookLoginBtn
+                appId="259668991860796"
+                autoLoad={true}
+                fields="name,email,picture"
+                onClick={this.componentClicked}
+                callback={this.responseFacebook} />
+              
+            );
+
+        return(
+            <>
+              {facebookData}
+
+            </>
+            
+        );
+    }
+}
+
+export default Facebook;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,12 +2,14 @@ import React from 'react';
 import "../styles/Header.css";
 import Nav from './NavBar.js';
 import logo from "../../src/logo.svg";
+import Facebook from './Facebook';
 
 function Header () {
     return (
       <div className="header1">
        <img src={logo} className="App-logo" alt="logo" />
         <Nav />
+        <Facebook />
       </div>
     )
   }
