43b9d29876d4ef72d0b23d9c272c8a6baa1c0f4c
philipipara <54330369+philipipara@users.noreply.github.com>
2020-04-16 07:13:02 -0700

    updated Register to show error if no info entered

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,18 +1,25 @@
 import React from "react";
 import API from '../utils/api';
 import '../styles/style.css';
+import axios from "axios";
+import { Link } from "react-router-dom";
+
 
 
 
 class Register extends React.Component {
+   
      state = {
         user: [],
+        
 
         fName: "",
         lName: "",
         email: "",
         password: ""
      }
+    
+     
    
     addUser = () => {
         const data = {
@@ -21,12 +28,41 @@ class Register extends React.Component {
          email: this.state.email,
          password: this.state.password
         }
-        console.log(this.state.user);
-        API.addUser(data).then(res => {
-            console.log(res)
-        })
+               
+        if(data.fName == "") {
+            this.setState({errorMessage: true, errorMessage2: false, errorMessage3: false, errorMessage4: false}, function(){
+                console.log(this.state);
+            })
+            return;
+        
+        } 
+        if(data.lName == ""){
+            this.setState({errorMessage: false, errorMessage2: true, errorMessage3: false, errorMessage4:false})
+            return;
+        } 
+
+        if(data.email == ""){
+            this.setState({errorMessage: false, errorMessage2: false, errorMessage3: true, errorMessage4:false})
+            return;
+        } 
+        if(data.password == ""){
+            this.setState({errorMessage: false, errorMessage2: false, errorMessage3: false, errorMessage4:true})
+            return;
+        } 
+        // API.addUser(data).then(res => {
+        //     console.log(res)
+        // })
+
     
     }
+
+    componentDidMount() {
+        // axios.get('http://localhost:3000/api/users/register')
+        //   .then(res => this.setState({user: res.data}))
+        //   .catch(err => { 
+        //     this.setState({errorMessage: err.message});
+        //   })
+      }
 
  
      handleChange= (e) => {
@@ -39,6 +75,8 @@ class Register extends React.Component {
          e.preventDefault();
         this.addUser();
 
+
+
         
      }
      render() {
@@ -49,32 +87,39 @@ class Register extends React.Component {
                  <div className="form-group">
                      <label>First Name</label>
                      <input className="neuflip" type="text" name="fName" value={this.fName} onChange={this.handleChange} />
-                         <div className="invalid-feedback">First Name is required</div>
+                     { this.state.errorMessage &&
+                         <div className="text-danger">First Name is required
+                           </div> }     
                  </div>
                  <div className="form-group">
                      <label>Last Name</label>
                      <input className="neuflip" type="text" name="lName" value={this.lName} onChange={this.handleChange}  />
-                     
-                         <div className="invalid-feedback">Last Name is required</div>
+                     { this.state.errorMessage2 &&
+                         <div className="text-danger">Last Name is required
+                           </div> } 
                      
                  </div>
                  <div className="form-group">
                      <label>Enter Email</label>
                      <input className="neuflip" type="text" name="email" value={this.email} onChange={this.handleChange} />
-                     
-                         <div className="invalid-feedback">Username is required</div>
+                     { this.state.errorMessage3 &&
+                         <div className="text-danger">Email is required
+                          </div> } 
+                        
                      
                  </div>
                  <div className="form-group">
                      <label>Password</label>
                      <input className="neuflip" type="password" name="password" value={this.password} onChange={this.handleChange}  />
-                         <div className="invalid-feedback">Password is required</div>
+                     { this.state.errorMessage4 &&
+                         <div className="text-danger">Password is required
+                           </div> } 
                  </div>
                  <div className="form-group">
-                     <button className="btn buttonz btn-primary neu text-dark">
+                     <button className="btn buttonz btn-primary neu text-dark" >
                          Register
                      </button>
-                     {/* <Link to="/login" className="btn btn-link">Cancel</Link> */}
+                     <Link to="/home" className="btn btn-link">Cancel</Link>
                  </div>
              </form>
          </div>
