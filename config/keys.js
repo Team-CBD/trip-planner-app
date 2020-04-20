e47b7532e8abb732613a415eb19f709674d0e5e5
Aidesiderio Alhambra <deri.alhambra@gmail.com>
2020-04-20 02:11:37 -0700

    heroku push test3

--- config/keys.js
+++ config/keys.js
@@ -2,3 +2,8 @@ module.exports = {
     "mongoURI": process.env.MONGODB_URI || 'mongodb://travelynx.herokuapp.com',
     "jwtSecret": "TL_myJwtSecret"
 }
+
+// {
+//     "mongoURI": "process.env.MONGOLAB_URI_BRONZE || 'mongodb://localhost/travelynx'",
+//     "jwtSecret": "TL_myJwtSecret"
+// }
