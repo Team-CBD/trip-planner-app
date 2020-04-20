982c234b14177e794ba8db8f60962bfda732c0e5
Aidesiderio Alhambra <deri.alhambra@gmail.com>
2020-04-20 02:20:40 -0700

    test4

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,16 @@
-module.exports = {
-    "mongoURI": process.env.MONGODB_URI || 'mongodb://travelynx.herokuapp.com',
-    "jwtSecret": "TL_myJwtSecret"
-}
+// module.exports = {
+//     "mongoURI": process.env.MONGODB_URI || 'mongodb://travelynx.herokuapp.com',
+//     "jwtSecret": "TL_myJwtSecret"
+// }
+// module.exports = {
+//     "mongoURI": "process.env.MONGODB_URI || 'mongodb://travelynx:travelynx1@ds029811.mlab.com:29811/heroku_kmkkd2vt'",
+//     "jwtSecret": "TL_myJwtSecret"
+// }
+
+// {
+//     "mongoURI": "'mongodb://travelynx:travelynx1@ds029811.mlab.com:29811/heroku_kmkkd2vt' || 'mongodb://localhost/travelynx'",
+//     "jwtSecret": "TL_myJwtSecret"
+// }
 
 // {
 //     "mongoURI": "process.env.MONGOLAB_URI_BRONZE || 'mongodb://localhost/travelynx'",
